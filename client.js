db66aab56fbec11f53c13db09469532f2b4031f8
Kobi Jones <kobijonesmail@gmail.com>
2023-04-03 18:19:43 -0500

    Add profile picture rendering

--- client.js
+++ client.js
@@ -1,1 +1,20 @@
-console.log('Here are all the available people:', people);
+$(document).ready(function () {
+  renderPictures();
+});
+
+function renderPictures() {
+  const jQElem = $('#pictures');
+  jQElem.empty();
+
+  people
+    .map(person => githubProfileURL(person.githubUsername))
+    .forEach(url => {
+      jQElem.append(`
+        <img src="${url}">
+      `);
+    });
+};
+
+function githubProfileURL(username) {
+  return `https://github.com/${username}.png?size=250`;
+}
