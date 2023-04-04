04b71e6bb51226c1b48665c90cb9109747667292
Kobi Jones <kobijonesmail@gmail.com>
2023-04-03 21:10:05 -0500

    Notify user of correct guess

--- client.js
+++ client.js
@@ -1,21 +1,42 @@
 const state = {
   people,
-  currentPersonIndex: Math.floor(Math.random() * people.length)
+  currentPersonIndex: Math.floor(Math.random() * people.length),
+  guessing: true
 };
 
 $(document).ready(function () {
-  renderCurrentPerson();
+  renderPrompt();
   renderPictures();
 
   $('#pictures').on('click', 'img', function(e) {
+    if (!state.guessing) return;
+
     const index = $(e.target).index();
     if (index === state.currentPersonIndex) {
-      state.currentPersonIndex = Math.floor(Math.random() * state.people.length);
+      // Hide other pictures
+      $('.picture').addClass('hide');
+      $(e.target).removeClass('hide');
+
+      state.guessing = false;
+      renderPrompt();
+
+      // Reset after cooldown
+      setTimeout(function() {
+        // Reset animation
+        $('.picture').removeClass('hide');
+
+        // Reset game
+        state.currentPersonIndex = Math.floor(Math.random() * state.people.length);
+        state.guessing = true;
+        renderPrompt();
+      }, 2000);
     } else {
+      // Wiggle picture on incorrect guess
       $(e.target).addClass('wiggle');
+      // Reset animation after running
       setTimeout(() => $(e.target).removeClass('wiggle'), 400);
     }
-    renderCurrentPerson();
+    renderPrompt();
   });
 });
 
@@ -27,14 +48,19 @@ function renderPictures() {
     .map(person => githubProfileURL(person.githubUsername))
     .forEach(url => {
       jQElem.append(`
-        <img src="${url}">
+        <img class="picture" src="${url}">
       `);
     });
 }
 
-function renderCurrentPerson() {
-  const jQElem = $('#guess-name');
-  jQElem.text(state.people[state.currentPersonIndex].name);
+function renderPrompt() {
+  const jQPrompt = $('#prompt');
+  jQPrompt.empty();
+  const text = state.guessing
+    ? `Click on ${state.people[state.currentPersonIndex].name}`
+    : 'Correct!';
+
+  jQPrompt.append(`<h1>${text}</h1>`);
 }
 
 function githubProfileURL(username) {
